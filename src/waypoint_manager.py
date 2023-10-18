e7b70c6fedffde241eb77cd92a22d367318c316e
alf767443 <andreluisfrana98@gmail.com>
2023-10-18 15:25:51 +0100

    Change status handle

--- src/waypoint_manager.py
+++ src/waypoint_manager.py
@@ -126,7 +126,7 @@ class ReadCSV_Waypoint_List():
             try:
                 while True:
                     move_base_status = rospy.wait_for_message('/move_base/status', GoalStatusArray, timeout=2)
-                    print(move_base_status)
+                    # print(move_base_status)
                     if move_base_status.status_list[0].status == 1:
                         delta_time = move_base_status.header.stamp.secs - move_base_status.status_list[0].goal_id.stamp.secs
                         if delta_time > max_wait_to_reached:
@@ -136,7 +136,7 @@ class ReadCSV_Waypoint_List():
                         rospy.loginfo(f"Goal reached... Next goal")
                         break
                     else:
-                        rospy.logwarn(f"Status not reconized: {str(move_base_status.status_list[0])}")
+                        rospy.logdebug(f"Status not reconized: {str(move_base_status.status_list[0])}")
 
                     rospy.sleep(0.5)
             except rospy.exceptions.ROSException:
